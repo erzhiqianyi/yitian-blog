692f88dfbb071347fc048d60b4b4d8dd89d6664a
二之前一 <github@caofeng.me>
2019-07-05 09:02:56 +0800

    install i18n

--- blog-vue/src/main.js
+++ blog-vue/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
