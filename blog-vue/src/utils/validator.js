6226e925178a426d85644691c2000a06690c249c
erzhiqianyi <github@caofeng.me>
2019-07-07 22:36:22 +0800

    validate

---
+++ blog-vue/src/utils/validator.js
@@ -0,0 +1,15 @@
+import i18n from '@/locales' // internationalization
+
+export function validEmail(rule, value, callback) {
+    const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (!reg.test(value)) {
+        callback(new Error(i18n.t('feedback.email_format')));
+    } else {
+        callback()
+    }
+
+}
+
+
+
+
--- blog-vue/src/views/pages/register/index.vue
+++ blog-vue/src/views/pages/register/index.vue
@@ -3,29 +3,31 @@
         <div class="container">
             <div class="form">
                 <h1>{{$t("system.name")}}</h1>
-                <el-form ref="form" :model="register" label-width="100px">
-                    <el-form-item :label='$t("register.name")' :required=true>
+                <el-form ref="form" :model="register" status-icon :rules="rules" label-width="100px">
+                    <el-form-item :label='$t("register.name")' :required=true prop="name">
                         <el-input v-model="register.name" :placeholder='$t("register.name")'></el-input>
                     </el-form-item>
-                    <el-form-item :label='$t("register.email")' :required=true>
+                    <el-form-item :label='$t("register.email")' :required=true prop="email">
                         <el-input v-model="register.email" :placeholder='$t("register.email")'></el-input>
                     </el-form-item>
                     <el-form-item :label='$t("register.password")' :required=true>
-                        <el-input v-model="register.password" :placeholder='$t("register.password")' type="password"></el-input>
+                        <el-input v-model="register.password" :placeholder='$t("register.password")'
+                                  type="password"></el-input>
                     </el-form-item>
                     <el-form-item :label='$t("register.re_password")' :required=true>
-                        <el-input v-model="register.rePassword" :placeholder='$t("register.re_password")' type="password"></el-input>
+                        <el-input v-model="register.rePassword" :placeholder='$t("register.re_password")'
+                                  type="password"></el-input>
                     </el-form-item>
                     <el-form-item :label='$t("register.code")' :required=true>
                         <el-col :span="10">
-                            <el-input v-model="register.code" :placeholder='$t("register.code")' ></el-input>
+                            <el-input v-model="register.code" :placeholder='$t("register.code")'></el-input>
                         </el-col>
                         <el-col :span="10" class="ml-10">
                             <el-button type="primary" @click="onSubmit">{{$t("register.get_code")}}</el-button>
                         </el-col>
                     </el-form-item>
                     <el-form-item :label='$t("register.protocol")' :required=true>
-                        <el-switch v-model="register.protocol" active-color="#13ce66" ></el-switch>
+                        <el-switch v-model="register.protocol" active-color="#13ce66"></el-switch>
                         <el-form-item>
                             <el-link type="primary">{{$t("register.agree_protocol")}}</el-link>
                         </el-form-item>
@@ -44,10 +46,12 @@
 </template>
 
 <script>
+    import {validEmail} from "@/utils/validator";
+
     export default {
         name: "Register",
         data() {
-            return {
+           return {
                 register: {
                     name: '',
                     email: '',
@@ -55,6 +59,17 @@
                     rePassword: '',
                     code: '',
                     protocol: true
+                },
+                rules: {
+                    name: [
+                        {required: true, message: this.$t("feedback.enter_name"), trigger: 'blur'},
+                        {min: 3, max: 20, message: this.$t("feedback.name_length"), trigger: 'blur'}
+                    ],
+                    email: [
+                        {required: true, message: this.$t("feedback.enter_email"), trigger: 'blur'},
+                        {min: 3, max: 30, message: this.$t("feedback.email_length"), trigger: 'blur'},
+                        {validator: validEmail, trigger: 'blur'}
+                    ]
                 }
             }
         },
