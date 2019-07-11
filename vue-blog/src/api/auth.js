67a79897575daaec8f2f8942a2c0e684cedcc106
二之前一 <github@caofeng.me>
2019-07-11 08:03:11 +0800

    delete old vue blog version

--- vue-blog/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/http'
-
-export function loginByPassword(data) {
-    return request({
-        url: '/api/auth/login/password',
-        method: 'post',
-        data
-    })
-}
