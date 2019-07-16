205b41608b72bd94e09e60effc04f12ba46b8ee9
二之前一 <github@caofeng.me>
2019-07-16 09:04:39 +0800

    validate register param

--- spring-boot-blog/src/main/java/com/erzhiqianyi/blog/controller/auth/RegisterController.java
+++ spring-boot-blog/src/main/java/com/erzhiqianyi/blog/controller/auth/RegisterController.java
@@ -7,6 +7,7 @@ import com.erzhiqianyi.blog.model.vo.auth.UserVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,7 @@ public class RegisterController {
             produces = SwaggerConstant.MEDIA_JSON,
             response = UserVo.class
     )
-    public Mono<ResponseResult<UserVo>> registerByEmail(@RequestBody EmailRegisterRequest request) {
+    public Mono<ResponseResult<UserVo>> registerByEmail(@RequestBody @Validated EmailRegisterRequest request) {
         return Mono.just(ResponseResult.success(new UserVo(request)));
     }
 
