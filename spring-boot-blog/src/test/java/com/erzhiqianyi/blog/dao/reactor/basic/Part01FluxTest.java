3d63275faba5f3705966d3c007c6619560ea1067
erzhiqianyi <github@caofeng.me>
2019-07-20 22:51:25 +0800

    create flux instance and test

---
+++ spring-boot-blog/src/test/java/com/erzhiqianyi/blog/dao/reactor/basic/Part01FluxTest.java
@@ -0,0 +1,53 @@
+package com.erzhiqianyi.blog.dao.reactor.basic;
+
+import org.junit.Before;
+import org.junit.Test;
+import reactor.test.StepVerifier;
+
+public class Part01FluxTest {
+    Part01Flux part01Flux;
+
+    @Before
+    public void init() {
+        part01Flux = new Part01Flux();
+    }
+
+    @Test
+    public void emptyFlux() {
+        StepVerifier.create(part01Flux.emptyFlux())
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void fooBarFluxFromValues() {
+        StepVerifier.create(part01Flux.fooBarFluxFromValues())
+                .expectNext("foo", "bar")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void fooBarFluxFromList() {
+        StepVerifier.create(part01Flux.fooBarFluxFromList())
+                .expectNext("foo", "bar")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void errorFlux() {
+        StepVerifier.create(part01Flux.errorFlux())
+                .expectError(IllegalStateException.class)
+                .verify();
+    }
+
+    @Test
+    public void counter() {
+        StepVerifier.create(part01Flux.counter())
+                .expectNext(0l,1l,2l,3l,4l,5l,6l,7l,8l,9l)
+                .expectComplete()
+                .verify();
+
+    }
+}
