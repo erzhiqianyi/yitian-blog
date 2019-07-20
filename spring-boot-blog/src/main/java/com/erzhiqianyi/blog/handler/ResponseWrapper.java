b634dbf9ab56b503080274f03d5d5467b126e19b
erzhiqianyi <github@caofeng.me>
2019-07-20 18:53:28 +0800

    delete log

--- spring-boot-blog/src/main/java/com/erzhiqianyi/blog/handler/ResponseWrapper.java
+++ spring-boot-blog/src/main/java/com/erzhiqianyi/blog/handler/ResponseWrapper.java
@@ -1,19 +1,17 @@
 package com.erzhiqianyi.blog.handler;
 
 import com.erzhiqianyi.blog.model.vo.ResponseResult;
-import lombok.extern.log4j.Log4j2;
-import org.assertj.core.util.Preconditions;
-import org.springframework.core.MethodParameter;
-import org.springframework.http.codec.HttpMessageWriter;
-import org.springframework.web.reactive.HandlerResult;
-import org.springframework.web.reactive.accept.RequestedContentTypeResolver;
-import org.springframework.web.reactive.result.method.annotation.ResponseBodyResultHandler;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
+        import org.assertj.core.util.Preconditions;
+        import org.springframework.core.MethodParameter;
+        import org.springframework.http.codec.HttpMessageWriter;
+        import org.springframework.web.reactive.HandlerResult;
+        import org.springframework.web.reactive.accept.RequestedContentTypeResolver;
+        import org.springframework.web.reactive.result.method.annotation.ResponseBodyResultHandler;
+        import org.springframework.web.server.ServerWebExchange;
+        import reactor.core.publisher.Mono;
 
-import java.util.List;
+        import java.util.List;
 
-@Log4j2
 public class ResponseWrapper extends ResponseBodyResultHandler {
 
     private static MethodParameter param;
@@ -50,7 +48,7 @@ public class ResponseWrapper extends ResponseBodyResultHandler {
     @Override
     @SuppressWarnings("unchecked")
     public Mono<Void> handleResult(ServerWebExchange exchange, HandlerResult result) {
-        //校验返回结果不能为空
+        //校验返回结果不能为null
         Preconditions.checkNotNull(result.getReturnValue(), "response is null!");
         // 将结果包装成统一返回结果 ResponseResult
         Mono<ResponseResult> body =
