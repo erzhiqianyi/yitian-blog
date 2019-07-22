e578ab3044af91ce47a2ba190ca4f3f3867f9775
二之前一 <github@caofeng.me>
2019-07-23 07:55:36 +0800

    verify jwt

--- spring-boot-blog/src/main/java/com/erzhiqianyi/blog/model/bo/auth/AuthUserBo.java
+++ spring-boot-blog/src/main/java/com/erzhiqianyi/blog/model/bo/auth/AuthUserBo.java
@@ -9,18 +9,21 @@ import java.time.Instant;
 import java.time.temporal.TemporalUnit;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Objects;
+
+import static java.time.temporal.ChronoField.INSTANT_SECONDS;
 
 @Getter
 public class AuthUserBo {
     private String name;
-    private Date issueAt;
-    private Date expirationAt;
+    private Instant issueAt;
+    private Instant expirationAt;
 
     public AuthUserBo(String name, long expiration, TemporalUnit unit) {
         this.name = name;
         Instant now = Instant.now();
-        this.issueAt = Date.from(now);
-        this.expirationAt = Date.from(now.plus(expiration, unit));
+        this.issueAt = now;
+        this.expirationAt = now.plus(expiration, unit);
     }
 
     public AuthUserBo(String token, String secret) {
@@ -28,18 +31,33 @@ public class AuthUserBo {
                 .setSigningKey(Base64.getEncoder().encodeToString(secret.getBytes()))
                 .parseClaimsJws(token)
                 .getBody();
+        this.name = claims.getSubject();
+        this.issueAt = claims.getIssuedAt().toInstant();
+        this.expirationAt = claims.getExpiration().toInstant();
     }
 
-    public String token(String secret) {
+    public String token(SignatureAlgorithm alg,String secret) {
         secret = Base64.getEncoder().encodeToString(secret.getBytes());
         return Jwts.builder()
                 .setSubject(name)
-                .setIssuer(name)
-                .setIssuedAt(issueAt)
-                .setExpiration(expirationAt)
-                .signWith(SignatureAlgorithm.HS512, secret)
+                .setIssuedAt(Date.from(issueAt))
+                .setExpiration(Date.from(expirationAt))
+                .signWith(alg, secret)
                 .compact();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AuthUserBo)) return false;
+        AuthUserBo that = (AuthUserBo) o;
+        return getName().equals(that.getName()) &&
+                getIssueAt().getLong(INSTANT_SECONDS) == that.getIssueAt().getLong(INSTANT_SECONDS) &&
+                getExpirationAt().getLong(INSTANT_SECONDS) == that.getExpirationAt().getLong(INSTANT_SECONDS);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getIssueAt().getLong(INSTANT_SECONDS), getExpirationAt().getLong(INSTANT_SECONDS));
+    }
 }
--- spring-boot-blog/src/test/java/com/erzhiqianyi/blog/model/bo/auth/AuthUserBoTest.java
+++ spring-boot-blog/src/test/java/com/erzhiqianyi/blog/model/bo/auth/AuthUserBoTest.java
@@ -1,9 +1,13 @@
 package com.erzhiqianyi.blog.model.bo.auth;
 
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 
 import java.time.temporal.ChronoUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @Log4j2
 public class AuthUserBoTest {
@@ -11,6 +15,8 @@ public class AuthUserBoTest {
     @Test
     public void token() {
         AuthUserBo authUser = new AuthUserBo("Test", 2, ChronoUnit.HOURS);
-        log.info(authUser.token("2356fd"));
+        String jwt = authUser.token(SignatureAlgorithm.HS512 ,"Test");
+        AuthUserBo jwtUser = new AuthUserBo(jwt, "Test");
+        assertTrue(authUser.equals(jwtUser));
     }
 }
